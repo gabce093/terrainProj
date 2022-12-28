b8fd3cd92ca941296e60e84aebb8a115a4fe4707
vicim994 <72468847+vicim994@users.noreply.github.com>
2022-12-28 13:19:58 +0100

    Created terrain texture + added some postprocessing

--- main.js
+++ main.js
@@ -9,6 +9,12 @@ import { CanvasTexture } from 'three';
 import { OBJLoader } from './node_modules/three/examples/jsm/loaders/OBJLoader.js';
 import { MTLLoader } from './node_modules/three/examples/jsm/loaders/MTLLoader.js';
 import { fbm, ridgedFbm } from './fbm';
+import { RenderPass } from './node_modules/three/examples/jsm/postprocessing/RenderPass';
+import { EffectComposer } from './node_modules/three/examples/jsm/postprocessing/EffectComposer';
+import { UnrealBloomPass } from './node_modules/three/examples/jsm/postprocessing/UnrealBloomPass';
+import { HalftonePass } from './node_modules/three/examples/jsm/postprocessing/HalftonePass';
+
+
 
 const noise = createNoise2D();
 const renderer = new THREE.WebGLRenderer();
@@ -18,6 +24,15 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.toneMappingExposure = 0.5;
 document.body.appendChild(renderer.domElement);
 
+//Red lights
+const red_ctx = document.createElement('canvas').getContext('2d');
+red_ctx.canvas.width = 40;
+red_ctx.canvas.height = 40;
+red_ctx.fillStyle = 'red';
+red_ctx.fillRect(0,0,40,40);
+var red_lights = new CanvasTexture(red_ctx.canvas);
+ 
+
 //progress console output when loading car
 const onProgress = function ( xhr ) {
   if ( xhr.lengthComputable ) {
@@ -26,37 +41,56 @@ const onProgress = function ( xhr ) {
   }
 };
 //Loads the car
-
 function loadCar(onOff){ 
   if(onOff){
     new MTLLoader()
     .setPath( '' )
     .load( 'CyberpunkDeLorean.mtl', function ( materials ) {
       materials.preload();
+      console.log(materials)
+      materials.materials.DeLorean_RedLight.emissiveIntensity = 1000;
+      //materials.materials.DeLorean_RedLight.lightMap = red_lights;
+      //materials.materials.DeLorean_RedLight.lightMapIntensity = 100;
       new OBJLoader()
         .setMaterials( materials )
         .setPath( '' )
         .load( 'CyberpunkDeLorean.obj', function ( car ) {
-          car.position.set( 0.0,6.0,950 );
+          car.position.set( 0.0,6.0,960);
+          //car.scale.set(0.5,0.5,0.5);
           var Rot = new THREE.Euler( Math.PI/2.0, Math.PI/1.0 , 0.0, 'XYZ' );
           car.rotation.copy(Rot);
           car.name = 'car';
           scene.add( car );
-         
         }, onProgress );
     } );
   }
   scene.remove(scene.getObjectByName('car'));
   }
 
-  //turn car on or off
-  var controller = new function() {  
-    this.onOrOff=false;
-  }
+//turn car on or off
+var controller = new function() {  
+  this.onOrOff=false;
+}
 
-//scene
+//Scene
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 2500);
+
+const renderScene = new RenderPass(scene, camera);
+const composer = new EffectComposer(renderer);
+composer.addPass(renderScene);
+
+//Post-Processing
+const bloompass = new UnrealBloomPass(
+  new THREE.Vector2(window.innerWidth, window.innerHeight),
+  0.4,
+  0,
+  0.5,
+);
+//composer.addPass(bloompass);
+
+const halftone = new HalftonePass(window.innerWidth, window.innerHeight);
+//composer.addPass(halftone);
 
 //const controls = new OrbitControls(camera, renderer.domElement);
 //controls.update() must be called after any manual changes to the camera's transform
@@ -64,11 +98,12 @@ const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerH
 camera.position.set(0, 20, 1000);
 
 scene.add(new THREE.AmbientLight(0x000055, 0.5));
-var light = new THREE.PointLight(0xe65c00, 1);
+var light = new THREE.PointLight(0xe65c00, 2);
 scene.add(light);
 light.position.z = -2000;
 light.position.x = 0;
 light.position.y = 200;
+
 
 //Sky-Shader
 let skyShader = new Sky();
@@ -83,9 +118,9 @@ skyShader.material.uniforms.rayleigh.value = 5;
 skyShader.material.uniforms.mieDirectionalG.value = 0.5;
 
 var light = new THREE.PointLight(0xffcc77, 1);
-scene.add(light);
-light.position.z = -1000;
-light.position.y = 150;
+//scene.add(light);
+light.position.z = 960;
+light.position.y = 100;
 
 //Fog
 const color = 0xFFFFFF;  // white
@@ -93,13 +128,44 @@ const near = 1680;
 const far = 1700;
 //scene.fog = new THREE.Fog(color, near, far);
 
-//Plane
+//Terrain
 const planeWidth = 2000*1.5;
 const planeHeight = 2000*1.5;
 var geometry = new THREE.PlaneGeometry(planeWidth, planeHeight,256, 256);
-var material = new THREE.MeshPhongMaterial();
+
+//Terrain material
+const box_ctx = document.createElement('canvas').getContext('2d');
+box_ctx.canvas.width = 80;
+box_ctx.canvas.height = 80;
+box_ctx.fillStyle = 'black';
+box_ctx.fillRect(0,0,80,80);
+box_ctx.fillStyle = '#BC13FE';
+box_ctx.fillRect(box_ctx.canvas.width/2-0.5, 0, 1, box_ctx.canvas.height);
+box_ctx.fillRect(0, box_ctx.canvas.height/2-0.5, box_ctx.canvas.width, 1);
+
+const terrain_ctx = document.createElement('canvas').getContext('2d');
+document.body.appendChild(box_ctx.canvas);
+terrain_ctx.canvas.width = 256*16;
+terrain_ctx.canvas.height = 256*16;
+var pattern = box_ctx.createPattern(box_ctx.canvas, 'repeat');
+terrain_ctx.fillStyle = pattern;
+terrain_ctx.fillRect(0, 0, terrain_ctx.canvas.width, terrain_ctx.canvas.height);
+
+var terrain_map = new CanvasTexture(terrain_ctx.canvas)
+terrain_map.wrapT = THREE.RepeatWrapping;
+terrain_map.repeat.y = 1;
+
+var material = new THREE.MeshPhongMaterial({
+  shininess: 40,
+  lightMap: terrain_map,
+  lightMapIntensity: 10,
+  //emissiveMap: terrain_map, 
+  //emissive: '#BC13FE',
+});
 
 var plane = new THREE.Mesh(geometry, material);
+const uv1Array = plane.geometry.getAttribute("uv").array;
+plane.geometry.setAttribute( 'uv2', new THREE.BufferAttribute( uv1Array, 2 ) );
 
 //Highway
 const highway_size = 25;
@@ -115,10 +181,12 @@ texture.repeat.y = 1;
 //texture.minFilter = THREE.NearestFilter;
 
 function drawHighway() {
-  color_ctx.fillStyle = 'black';
+  color_ctx.fillStyle = 'blackk';
   color_ctx.fillRect(0, 0, color_ctx.canvas.width, color_ctx.canvas.height);
   const x = 40;
-  color_ctx.fillStyle = '#fff2ca';
+  color_ctx.fillStyle = '#BC13FE';
+  color_ctx.fillRect(0, 0, 3, color_ctx.canvas.height);
+  color_ctx.fillRect(color_ctx.canvas.width-3, 0, 3, color_ctx.canvas.height);
 
   for(var y = 0; y < color_ctx.canvas.height; y += 8) {
     color_ctx.fillRect(x, y, 3, 5);
@@ -150,22 +218,23 @@ function drawBumpMap() {
 drawBumpMap();
 
 var highway_material = new THREE.MeshPhongMaterial({
-  map: texture, 
+  //map: texture, 
   bumpMap: bump_map, 
-  emissiveMap: texture, 
-  emissive: 'white',
+  lightMap: texture,
+  lightMapIntensity: 1,
+  //reflectivity: 100,
 })
 var highway = new THREE.Mesh(highway_geometry,highway_material);
+const uv1Array2 = highway.geometry.getAttribute("uv").array;
+highway.geometry.setAttribute( 'uv2', new THREE.BufferAttribute( uv1Array2, 2 ) );
 
 //Add to scene
-scene.add( skyShader );
-scene.add(highway);
 highway.rotation.x = -Math.PI / 2;
-scene.add(plane);
 plane.rotation.x = -Math.PI / 2;
 highway.position.y = 0.17;
-
-//Generate Terrain
+scene.add(plane);
+scene.add( skyShader );
+scene.add(highway);
 
 //Customizable variables
 var speed = {speed: 20};
@@ -179,6 +248,12 @@ var amplitudes = new function() {
 var ridged = {ridged: false};
 var overview = {overview: false};
 
+var postP = {
+  halftone: false,
+  bloom: false,
+}
+
+//Generate Terrain
 function computeTerrain(octav1,octav2,octav3,octav4,octav5, offset=0) {
   var octave_value = 0.002;
   var distance_from_highway = 0;
@@ -250,6 +325,10 @@ carFolder.add(controller, 'onOrOff').onChange(() => loadCar(controller.onOrOff))
 const overviewFolder = gui.addFolder('Overview');
 overviewFolder.add(overview, 'overview').onChange(() => changeCamera());
 
+const postprocessingFolder = gui.addFolder('Post-Processing');
+postprocessingFolder.add(postP, 'halftone').onChange((value) => value ? composer.addPass(halftone) : composer.removePass(halftone));
+postprocessingFolder.add(postP, 'bloom').onChange((value) => value ? composer.addPass(bloompass) : composer.removePass(bloompass));
+
 gui.updateDisplay();
 
 var clock = new THREE.Clock();
@@ -261,12 +340,17 @@ function animate() {
   var offset = clock.getElapsedTime()*speed.speed;
 
   //Update scene
-  texture.offset.y = offset*0.0004;
+  terrain_map.offset.y = offset*0.00035;
+  terrain_map.needsUpdate = true;
+  texture.offset.y = offset*0.00035;
   texture.needsUpdate = true;
+  bump_map.offset.y = offset*0.00035;
+  bump_map.needsUpdate = true;
   computeTerrain(amplitudes.octav1,amplitudes.octav2,amplitudes.octav3, amplitudes.octav4, amplitudes.octav5, offset);
 
   //controls.update();
 
-	renderer.render(scene, camera);
+  composer.render();
+	//renderer.render(scene, camera);
 }
 animate();
